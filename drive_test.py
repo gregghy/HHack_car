19da66624aa92d2a8fe54511a3a6919c77d2f5fa
gregghy <gregoriosilves@gmail.com>
2025-03-04 15:30:06 -0800

    get person direction

--- drive_test.py
+++ drive_test.py
@@ -45,6 +45,24 @@ def move_car(direction):
         GPIO.output(right_motor_forward, GPIO.LOW)
         GPIO.output(right_motor_backward, GPIO.LOW)
 
+
+# Function to detect the direction of the person
+def get_person_direction(landmarks, frame_width, frame_height):
+    if landmarks:
+        nose = landmarks[mp_pose.PoseLandmark.NOSE]
+        nose_x = int(nose.x * frame_width)
+        nose_y = int(nose.y * frame_height)
+
+        # Direction towards the person (left, right, forward)
+        if nose_x < frame_width // 3:
+            return "left"
+        elif nose_x > frame_width * 2 // 3:
+            return "right"
+        else:
+            return "forward"
+    return "no_person"
+
+
 # Initialize OpenCV
 cap = cv2.VideoCapture(0)
 
